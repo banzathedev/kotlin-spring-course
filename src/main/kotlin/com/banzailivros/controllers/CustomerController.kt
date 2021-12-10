db15c13508749ac4a2aadea5c0e3a726519b92bd
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-12-10 08:55:37 -0300

    feat: added put and delete methods also now get all acpets query params

--- src/main/kotlin/com/banzailivros/controllers/CustomerController.kt
+++ src/main/kotlin/com/banzailivros/controllers/CustomerController.kt
@@ -1,6 +1,7 @@
 package com.banzailivros.controllers
 
 import com.banzailivros.controllers.request.PostCustomerRequest
+import com.banzailivros.controllers.request.PutCustomerRequest
 import com.banzailivros.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -13,7 +14,10 @@ class CustomerController {
     var mId: Int = 0
 
     @GetMapping
-    fun getCustomer(): List<CustomerModel> {
+    fun getAll(@RequestParam name: String?): List<CustomerModel> {
+        name?.let {
+            return customers.filter { it.name.contains(name, true) }
+        }
         return customers
     }
 
@@ -24,4 +28,23 @@ class CustomerController {
         customers.add(foundCustomer)
     }
 
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.filter { it.id == id }.first()
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun updateCustomer(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+        customers.filter { it.id == id }.first().let { foundCustomer ->
+            foundCustomer.name = customer.name
+            foundCustomer.email = customer.email
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: String) {
+        customers.removeIf { it.id == id }
+    }
 }
---
+++ src/main/kotlin/com/banzailivros/controllers/request/PutCustomerRequest.kt
@@ -0,0 +1,7 @@
+package com.banzailivros.controllers.request
+
+data class PutCustomerRequest(
+    var name: String,
+
+    var email: String
+)
