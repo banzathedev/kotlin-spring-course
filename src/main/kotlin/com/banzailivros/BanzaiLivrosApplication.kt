257a8a0f86c255038a1ba712848fa4eb6bf0e407
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-12-09 22:03:05 -0300

    project setup

---
+++ src/main/kotlin/com/banzailivros/BanzaiLivrosApplication.kt
@@ -0,0 +1,11 @@
+package com.banzailivros
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BanzaiLivrosApplication
+
+fun main(args: Array<String>) {
+	runApplication<BanzaiLivrosApplication>(*args)
+}
---
+++ src/test/kotlin/com/banzailivros/BanzaiLivrosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banzailivros
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BanzaiLivrosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
